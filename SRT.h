85a6a15641c1a5617e1f6012a2eb37f21f6e6382
kesfc <kevinli20030305@gmail.com>
2024-04-04 20:30:57 -0400

    111

--- SRT.h
+++ SRT.h
@@ -1,13 +1,23 @@
-#ifndef SRT_H
+#ifndef SRT_H  // Include guard to prevent multiple inclusions
 #define SRT_H
+
 #include <string>
 #include <vector>  
 #include "algorithms.h"
 #include "Process.h"
+using namespace std;
 
 class SRT : public Algo {
 public:
-    SRT(std::string name, std::vector<Process> processes, int t_cs);
+    SRT(std::string name, vector<Process> processes, int t_cs, double alpha);
+    void ProcessArrival(Process& process) override;
+    void StartCpu(Process& process) override;
+    void FinishCpu(Process& process) override;
+    void FinishIO(Process& process) override;
     void newProcessRunCheck() override;
+private:
+    double alpha;
 };
+
+
 #endif
